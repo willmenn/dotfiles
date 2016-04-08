ba9c60105fb1060e3f43d224860e06381eb163bc
William Ahrons <william.ahrons@gmail.com>
2016-04-08 22:39:31 +0530

    add install script for dotfiles

---
+++ install.sh
@@ -0,0 +1,18 @@
+
+
+#Creating symbolic link for config files
+
+
+ln -sf $(pwd)/.gitconfig ~/.gitconfig
+
+ln -sf $(pwd)/.zshrc  ~/.zshrc
+
+ln -sf $(pwd)/.vimrc ~/.vimrc
+
+ln -sf $(pwd)/.vim  ~/.vim
+
+ln -sf $(pwd)/.bash_profile ~/.bash_profile
+
+ln -sf $(pwd)/.alias ~/.alias
+
+
