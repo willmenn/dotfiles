852d5f47424f924c476e2a90210086cff9276939
William Ahrons <william.ahrons@gmail.com>
2016-04-11 23:42:51 +0530

    add brew isntallation file

---
+++ brew.sh
@@ -0,0 +1,20 @@
+
+#Update brew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade
+
+brew cask install iterm2
+brew cask install google-chrome-canary
+
+brew install git
+brew install tree
+brew install node
+brew install zsh
+
+brew cleanup
+
+
+
+
