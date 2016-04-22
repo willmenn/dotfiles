0308381d7005b33797b92bcb3f0eb0353bad3637
William Ahrons <william.ahrons@gmail.com>
2016-04-22 13:36:43 -0300

    adding maven instalation

--- install.sh
+++ install.sh
@@ -10,10 +10,21 @@ mkdir .vim/files/swap
 mkdir .vim/files/info
 
 mkdir ~/Documents/Desenvolvimento
-mkdir ~/Documents/Desenvolvimento/Workspaces
+mkdir ~/Documents/Desenvolvimento/Workspace
+
+#Setting Maven
+mkdir ~/Documents/Desenvolvimento/Maven
+mkdir ~/Documents/Desenvolvimento/Maven/3.3.9
+if [ ! -f ~/Documents/Desenvolvimento/Maven/maven.zip ]; then
+
+curl -o ~/Documents/Desenvolvimento/Maven/maven.zip http://mirror.nbtelecom.com.br/apache/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.zip
+
+unzip ~/Documents/Desenvolvimento/maven/maven.zip -d ~/Documents/Desenvolvimento/Maven/3.3.9
+
+fi
+
 
 #Creating symbolic link for config files
-
 
 ln -sf $(pwd)/.gitconfig ~/.gitconfig
 
