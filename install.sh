75dc5f0e75e7dd36a088e0b40c4fd1a65e978790
William Ahrons <william.ahrons@gmail.com>
2016-04-26 15:38:59 -0300

    adding variables for more clean code

--- install.sh
+++ install.sh
@@ -8,38 +8,41 @@ mkdir .vim/files
 mkdir .vim/files/backup
 mkdir .vim/files/swap
 mkdir .vim/files/info
-
-mkdir ~/Documents/Desenvolvimento
-mkdir ~/Documents/Desenvolvimento/Workspace
+DEVFOLDER=~/Documents/Desenvolvimento
+mkdir $DEVFOLDER
+mkdir $DEVFOLDER/Workspace
 
 #Setting Maven
-mkdir -p ~/Documents/Desenvolvimento/Maven/3.3.9
-if [ ! -f ~/Documents/Desenvolvimento/Maven/maven.zip ]; then
+MAVENFOLDER=$DEVFOLDER/Maven
+mkdir -p $MAVENFOLDER/3.3.9
+if [ ! -f $MAVENFOLDER/maven.zip ]; then
 
-curl -o ~/Documents/Desenvolvimento/Maven/maven.zip http://mirror.nbtelecom.com.br/apache/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.zip
+curl -o $MAVENFOLDER/maven.zip http://mirror.nbtelecom.com.br/apache/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.zip
 
-unzip ~/Documents/Desenvolvimento/Maven/maven.zip -d ~/Documents/Desenvolvimento/Maven/3.3.9
+unzip $MAVENFOLDER/maven.zip -d $MAVENFOLDER/3.3.9
 
 fi
 
 #Setting Tomcat
-mkdir -p ~/Documents/Desenvolvimento/Tomcat/7.0.69
-if [ ! -f ~/Documents/Desenvolvimento/Tomcat/tomcat.zip ]; then
+TOMCATFOLDER=$DEVFOLDER/Tomcat
+mkdir -p $TOMCATFOLDER/7.0.69
+if [ ! -f $TOMCATFOLDER/tomcat.zip ]; then
 
-curl -o ~/Documents/Desenvolvimento/Tomcat/tomcat.zip	http://ftp.unicamp.br/pub/apache/tomcat/tomcat-7/v7.0.69/bin/apache-tomcat-7.0.69.zip
+curl -o $TOMCATFOLDER/tomcat.zip	http://ftp.unicamp.br/pub/apache/tomcat/tomcat-7/v7.0.69/bin/apache-tomcat-7.0.69.zip
 
-unzip ~/Documents/Desenvolvimento/Tomcat/tomcat.zip -d ~/Documents/Desenvolvimento/Tomcat/7.0.69
+unzip $TOMCATFOLDER/tomcat.zip -d $TOMCATFOLDER/7.0.69
 
-chmod +x ~/Documents/Desenvolvimento/Tomcat/7.0.69/apache-tomcat-7.0.69/bin/catalina.sh
+chmod +x $TOMCATFOLDER/7.0.69/apache-tomcat-7.0.69/bin/catalina.sh
 
 fi
 
 #Setting Ant Apache
-mkdir -p ~/Documents/Desenvolvimento/AntApache/1.9.7
-if [ ! -f ~/Documents/Desenvolvimento/AntApache/ant.zip ]; then
-	curl -o ~/Documents/Desenvolvimento/AntApache/ant.zip http://mirror.nbtelecom.com.br/apache//ant/binaries/apache-ant-1.9.7-bin.zip
+ANTFOLDER=$DEVFOLDER/AntApache
+mkdir -p $ANTFOLDER/1.9.7
+if [ ! -f $ANTFOLDER/ant.zip ]; then
+	curl -o $ANTFOLDER/ant.zip http://mirror.nbtelecom.com.br/apache//ant/binaries/apache-ant-1.9.7-bin.zip
 
-	unzip ~/Documents/Desenvolvimento/AntApache/ant.zip -d ~/Documents/Desenvolvimento/AntApache/1.9.7
+	unzip $ANTFOLDER/ant.zip -d $ANTFOLDER/1.9.7
 fi
 
 #Creating symbolic link for config files
