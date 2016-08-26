ca2bbf5f73a4229fc1568dfa8fce4d6e86e7db24
William Ahrons <william.ahrons@gmail.com>
2016-08-26 09:58:08 -0300

    Add gitignore global

--- install.sh
+++ install.sh
@@ -97,6 +97,8 @@ npm install --global git-recent
 
 ln -sf $(pwd)/.gitconfig ~/.gitconfig
 
+ln -sf $(pwd)/.gitignore_global ~/.gitignore_global
+
 ln -sf $(pwd)/.zshrc  ~/.zshrc
 
 ln -sf $(pwd)/.vimrc ~/.vimrc
